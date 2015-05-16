265a289901aa7be67ae31924e85506b58945d2ab
Matthew Ecklund <mecklund@ldschurch.org>
2015-05-16 10:22:49 -0600

    further along - starting step 4

--- app/app.js
+++ app/app.js
@@ -1,5 +1,37 @@
-var app = angular.module('directivePractice', []);
+var app = angular.module('directivePractice', ['ngRoute']);
 
-app.config(function(){
+app.config(function($routeProvider){
+	$routeProvider
+		.when('/home', {
+			templateUrl: 'app/home.html',
+			controller: 'homeCtrl'
+		})
+		.otherwise({
+			redirectTo: '/home'
+		})
 
 });
+
+app.directive('dirDisplay', function(){
+	return {
+		templateUrl: 'app/dirDisplay.html',
+		link: function(scope, element, attributes){
+			console.log(scope);
+			// console.log(element);
+			// console.log(attributes);
+			element.on('click', function(){
+				scope.displayAge = !scope.displayAge;
+				scope.displayEmail = !scope.displayEmail;
+				// console.log(scope.displayAge)
+				scope.$apply();
+			})
+		}
+	}
+});
+
+app.directive('dirWeather', function(){
+	return {
+		templateUrl: 'app/dirWeather.html'
+
+	}
+})
